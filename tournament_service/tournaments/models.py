2730aa0bcdafda7e199689fbb46714d8af41a079
Julian Kraus <>
2025-12-09 09:13:57 +0100

    add tournament participant and game models and serializers

--- tournament_service/tournaments/models.py
+++ tournament_service/tournaments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from players.models import Player
 
 class Tournament(models.Model):
     name = models.CharField(max_length=100)
@@ -6,3 +7,22 @@ class Tournament(models.Model):
 
     def __str__(self):
         return self.name
+
+class TournamentParticipant(models.Model):
+    tournament = models.ForeignKey(
+        Tournament, on_delete=models.CASCADE, related_name="participants"
+    )
+    player = models.ForeignKey(
+        Player, on_delete=models.CASCADE, related_name="tournament_participations"
+    )
+
+    class Meta:
+        unique_together = ("tournament", "player")
+
+
+class Game(models.Model):
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, related_name="games")
+    home_participant = models.ForeignKey(TournamentParticipant, on_delete=models.CASCADE, related_name="home_games")
+    away_participant = models.ForeignKey(TournamentParticipant, on_delete=models.CASCADE, related_name="away_games")
+    home_score = models.PositiveIntegerField()
+    away_score = models.PositiveIntegerField()
--- tournament_service/tournaments/serializers.py
+++ tournament_service/tournaments/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Tournament
+from .models import Tournament, TournamentParticipant, Game
 
 class TournamentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tournament
+        fields = "__all__"
+
+
+class TournamentParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TournamentParticipant
+        fields = "__all__"
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
         fields = "__all__"
